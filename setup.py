e36571bc49a94b3a18871bac53288100aa28af69
Chris Taylor <64110892+CJTAYL@users.noreply.github.com>
2025-03-19 05:51:50 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools 
+
+setuptools.setup(
+version='0.1.0',
+description='Simple package for testing purposes',
+author='Chris Taylor',
+author_email='CJTAYL2@gmail.com',
+packages=['proof_of_concept'],
+install_requires=['numpy'],
+)
